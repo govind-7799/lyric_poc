e90b4db66925654bb44a7c66a4ece07f8f7d7cf3
govind-7799 <78166225+govind-7799@users.noreply.github.com>
2021-05-28 14:10:50 +0530

    Create lyricsgenerator.py

---
+++ lyricsgenerator.py
@@ -0,0 +1,499 @@
+
+print(" \t\t\t\t\tWelcome\n")
+print("\t\t\tplease select one song from this playsits:\n")
+def get_lyrics():
+    print("\t\t\t1","I am an Indian")
+    print("\t\t\t2","Maguva Maguva")
+    print("\t\t\t3","Jalsa Jalsa")
+    print("\t\t\t4","Jennifer Lopez")
+    print("\t\t\t5","Baitikochi Choosthe")
+    print("\t\t\t6","Travelling Soldier")
+    print("\t\t\t7","Dekho Dekho Gabbar Singh")
+    print("\t\t\t8","Ee Reyi Teeyanidi")
+    print("\t\t\t9","Aakasham Nee Panja")
+    print("\t\t\t10","Ye Mera Jahan\n")
+
+while True:
+    get_lyrics()    
+    choice= input("\t\t\tEnter your song number :").lower()
+    if choice == "1":
+       print("""\t\t\tWhy do you worry where you born
+             or you there brother
+            when you know you are an Indian
+            Why do you worry about the color of your skin brother
+            When you know your blood is Indian
+            Why do you worry if you are North or South or Purab Paschim
+            when you know you are an Indian
+            you are an Indian
+           you are an Indian
+
+             Heyy
+
+           Ye desh ka pyara pyara kiski najar lagi ise
+Ye Bharat desh hamara kiski najar lagi ise
+gyajab ki ankh tara kiski najar lagi ise
+Ye tajan nakh na sara kiski najar lagi ise
+
+Ye khoon bhahahe apnon ka arey dekhona
+kya huwa hai sapno ka arey sochona
+harma ki chati sisak rakhi hai arey dekhona
+suhagatahe bahanon ka arey sochona
+
+I'm an Indian
+I'm an Indian
+
+Bhool gaye ho karmikon apne kaise Bharat vasi ho
+Bhool gaye Gandhi ke sapne kaise Bharat vasi ho
+Gear huwe hai sare apne kaise Bharat vasi ho
+roshni hai mata ki khere kaise Bharat vasi ho
+
+Ye desamegina endendu kalidina
+elugetti cheppey I'm an Indian
+gundellanni konde chesi kondallo current nimpi
+toda kotti cheppey I'm an Indian
+I'm an Indian
+I'm an Indian
+I'm an Indian
+
+Why don't you stop this evil killing and destruction
+Sing a song of freedom and love
+If you want to fight for liberation
+Choose the path that will make you survive
+Everybody join us together
+Don't you see the magic we can do
+This is the truth that India is all about
+Fight for us to make it and move
+I'm Indian
+You are Indian
+We are Indians
+We proud of India
+I'm Indian
+I'm proud of India
+We are Indians
+We are proud of India
+                """)
+    elif choice == "2" :
+         print("""\t\t\tMaguva maguva lokaniki thelusa nee viluva
+Maguva maguva nee sahananiki sarihuddulu kalava
+
+Atu itu anninta nuvve jagamantha
+Parugulu thisthavu inta bayata
+Alupani ravvantha anane anavanta
+Velugulu pusthavu velle dharantha
+
+Maguva maguva lokaniki thelusa nee viluva
+Maguva maguva nee sahananiki sarihuddulu kalava
+
+Nee katuku kanulu vipparakapothe
+Ee bhoomiki thelavaradhuga
+Nee gaajula cheyi kadalaadakapothe
+Ye manugada konasaagadugaa
+
+Prati varusalonu prema ga
+Allukunna bandhamaa anthuleni
+Nee shrama anchanaalakandunaa
+Aalayaalu korani aadishakti roopama
+
+
+Neevuleni jagathilo deepame velugunaa
+
+Needagu laalanalo priyamagu palanalo
+Prathi oka magavaadu pasivaadegaa
+Endari pedavulola ye chirunavvumma
+Aa siri merupulaku moolam nuvve gaa
+
+Maguva maguva lokaniki thelusa nee viluva
+Maguva maguva nee sahananiki sarihuddulu kalava
+                    """)
+    elif choice == "3":
+        print("""\t\t\tThey call him a cool cool angry man
+Super andhra thelusa
+Its the time for the dhol in the beat
+Come on come on karo jalsaa..aa..
+
+Jalsa jalsa Yo Yo Yo Yo
+Yo he is the man Yo the Jackie Chan
+He is the king of Andhra
+His place is super groovy Hyderbad
+And she is baby girl Sandra
+
+Hey sare gama pada nisa
+Arrey karo karo jara jalsa
+Hey sani dapa maga risa
+Arey karo karo jara jalsa (2x)
+
+Thelusa thelusa thelusa evarikaina thelusa
+Tsunami edhuruga vasthe ela kanabaduthundho
+Thelusa thelusa thelusa evvarikaina thelusa
+Tufane thalupulu thadithe ela vinapadtundho
+Arey theliaka pothe chudara babu
+He is the human tsunami
+Theliyalanukunte danger babu
+You’ve got to believe me
+
+Hey sare gama pada nisa
+Arey karo karo jara jalsa
+Hey sani dapa maga risa
+Arey karo karo jara jalsa
+
+Height enthuntado kolavalanipisthe
+Amanthamu ala ala Mount Everest avuthadu
+Fight em chestado ani saradha padite
+Strecher thanai sara sari ward ki cherustadu
+
+Arey gaddi pocha anukoni thunchadanikosthe
+Gadda para namilestadu
+Gundu sudhi chetikichi dhanda guchamante
+Konda tavvi parestadu
+
+Hey sare gama pada nisa
+Arrey karo karo jara jalsa
+Hey sani dapa maga risa
+Arey karo karo jara jalsa
+
+Manavadanukunte chelikadavudu
+Vimanamai bujalapa savaru cheyyistadu
+Paga vadhanukunte vilukadavuthadu
+Pramadhamai kshanalalo savalu puttisthadu
+
+Dhosadu pulani techipettamante
+Buttalani konukkostadu
+Yamapasam vachi peeka cuttukunte
+Dhani thoti vuyaluguthadu
+
+Hey sare gama pada nisa
+Arey karo karo jara jalsa
+Hey sani dapa maga risa
+Arey karo karo karo karo jara jalsa
+                    """)
+    elif choice == "4":
+        print("""\t\t\tJennifer Lopez Sketch Geesintattuga Vundiro Ee Sundari .. Come On
+Hey Britney Spears Ni Print Teesinattuga Vundiro Ee Cadburry ..
+Oh.. Nadume Chuste Shakira Daanni Antunkunna Cheyye Lucky Ra..
+Nadake Chuste Beyonce , Baby Navvindante Khallase ..
+Jeans Pant Vesukunna James Bond Gunnu Lanti Kannu Kotti Champamaakura..
+Black Belt Pettukuni Jackie Chan Laaga Nonchalk Tippamaakuro.
+
+Lady Kalla Lasere Nuvva , Paradise Flavoure Nuvva..
+Oxygen Nimpukunna Studentshangout Bombuva .. Saxophone Vampuve Nuvva ..
+Volcano Ki Best Friend Va .. Veyyi Volts High Current Va
+Vayasu Meeda Vaalutunna Tornado Nuvva .. Earthquake Thundere Nuvva
+Nee Rendu Kallu Radium Dialsaa, Nee Pedavulu Platinum Flowersaa ..
+Nuvvu Hello Ante Romancea , Nee Silence Ayina Violence Aa...
+Hey Titanic Heroine Part 2 Nuvvani Navvutunna Monalisa Mothukodaa
+Playboy Chupulunna Samurai Sumolanta Salaam Kottara ..
+
+Jennifer Lopez Sketch Geesintattuga Vundiro Ee Sundari ..
+Britney Spears Ni Print Teesinattuga Vundiro Ee Cadburry ..
+
+Dts Ringtone Va, Heart Shape Moonve Nuvva...
+Andamanna Software Cd-rom Va , Kammanaina Chloroform Va
+Romeo Ki Clone Ve Nuvva, Rainbow Ki Twin Ve Nuvva
+Dream University Ki Dean Ve Nuvva, Naa Zodiac Sign Ve Nuvva ..
+24 Carat Vanilla Nuvvu Hot Hot Mexican Taquilla
+Fully Loaded Raifel La Nannu Raid Chesaave Rambo La
+Madonnanu Banti Chesi Bouncer Vesi Nattugu, Pulse Rate Penchinave Frenchi Modala
+Maradona Lagipetti Goal Kottinattu Ga Flying Kissu Pettamaakalla..
+
+Jennifer Lopez Sketch Geesintattuga Vundiro Ee Sundari ..
+Britney Spears Ni Print Teesinattuga Vundiro Ee Cadburry ..""")
+    elif choice == "5":
+        print("""\t\t\tBaitikochi choosthe time emo 3’0 clock
+Intikelle 12B route mottham road block
+
+Baitikochi choosthe time emo 3’0 clock
+Intikelle 12B route mottham road block
+
+
+ 
+Oye nee chethikunna bangles ye
+Thalamesena sandles ye
+Walk way lo choosthe
+Puvvula rekkalu full ga kappese
+Corner lo coffee shop
+Vedi vedi ga visirese
+Bus kitiki dhaggara
+College student phone lo moge
+FM lo evvaro paadithe
+Vollantha endhuko voogene
+Apple pandula sooryude
+Aeroplane la naa gunde
+Thelindhe gaalilo mabbula
+Jaarindhe nelapai needala
+Volle gucchele sudden ga
+Challagaale villian laa
+
+Baitikochi choosthe time emo 3’0 clock
+Intikelle 12B route mottham road block
+
+Baitikochi choosthe time emo 3’0 clock
+Intikelle 12B route mottham road block
+
+
+ 
+Nee pakka nunna vela car horn kooda
+Classical music aa
+Ee mandutenda kooda AC jalluthondhi
+Nee navvuloni magic aa
+Taxi hire chesi nuvvu
+Beramaaduthunte cute gundhi basic ga
+Breaks vesinappudalla nee bugga nannu thaki
+Sorry cheppe naazugga
+Nuvvunna kitiki ye vaipo vethiki
+WhatsApp chesthava
+Mabbulni kadhipi mohamaata petti
+Chandhrudni thesthaga
+
+Baitikochi choosthe time emo 3’0 clock
+Intikelle 12B route mottham road block
+
+FM lo evvaro paadithe
+Vollantha endhuko voogene
+Apple pandula sooryude
+Aeroplane la naa gunde
+Thelindhe gaalilo mabbula
+Jaarindhe nelapai needala
+Volle gucchele sudden ga
+Challagaale villian laa
+                    """)
+    elif choice == "6":
+        print("""\t\t\tLook at my face in the mirror
+and i wonder what i see
+i’m just a traveling soldier
+and i’ll be all i can be
+but right now!!! i just wanna be free
+i wanna be all i can be
+but right now!!! i just wanna be free
+i wanna be all i can be
+
+don’t you say i’m a failure
+you don’t know who i can be
+if they wanna know who i am
+they just have to wait and see
+but right now i just wanna be free
+i wanna be all i can be
+
+look at my face in the mirror
+and i wonder what i see
+am just a traveling soldier
+and i’ll be all i can be
+but right now i just wanna be free
+i wanna be all i can be
+but right now i just wanna be free
+i wanna be all i can be hei hei
+i wanna be all i can be..
+
+i’m just a traveling soldier
+and i’ll be all i can be
+but right now i just wanna be free
+i wanna be all i can be
+but right now i just wanna be free
+i wanna be all i can be
+
+i’m just a traveling soldier
+and i’ll be all i can be!
+but right now,i just wanna be free
+i wanna be all i can be
+but right now,i just wanna be free
+i wanna be all i can be
+hey hey i wanna be all i can be
+hey hey i wanna be all i can be
+hey hey i wanna be all i can be
+                    """)
+    elif choice == "7":
+        print("""\t\t\tLadies And GentleMen Boys And Girls And All The Fans
+Here Comes The PowerKing
+We Call Him As GabbarSingh..
+
+Dekho Dekho GabbarSing
+All Indiaki HyperSingh
+Veedi Peru Vinte Gundaala Gundellona Gulla Sounding…
+
+Veedi Body Steel Casing
+Veedi Naram Nylon String
+Veedi Character Khaki Dress Ke Kotha Colouring…
+
+Satta Ke Spellinguu Yelele
+Kottado Swellinguu Yelele..
+Kallallo Firinguu Yelele
+Ye Villian Kina Pedda Warninguu…
+
+By Birth Ye Pudindguu Yelele
+PowerKe Brandinggu
+HighEnd Stylingu Yelele
+Veedi Following Ye Mind Blowinguu..
+
+GabbarSingh GabbarSingh
+He Z On The Way To Do Something..
+GabbarSingh GabbarSingh
+Its A Brand New Song To Sing..
+
+GabbarSingh GabbarSingh
+He Z On The Way To Do Something..
+GabbarSingh GabbarSingh
+Its A Brand New Song To Sing
+
+Mana Jolikoste Brotheru Mantekki Poddi Weatheru
+Mana Cheti Debba Tinnollu Malli Legaru….
+Manchodni Gilla Galaru Ahey Cheddodni Gicha Galaru
+Ye Lekkakandani Nalontodini Gelikedevaru…
+
+Megatavallaki Mokkadu
+Raffodu Taffodu
+Kurisinta Tikkodu Itta Puttestadu Wat To Do…
+Jo Dhar Gaya Samjho MarGaya …
+
+GabbarSingh GabbarSingh
+He Z On The Way To Do Something..
+GabbarSingh GabbarSingh
+Its A Brand New Song To Sing…
+
+Mana Facuu Picha Classu
+Mana Punchu Oora Maassu
+Ye Danger Ina Sare Yedureltayi Manalo Gutsuu…
+
+Mana Vonti Meeda Dressu
+Nippuki Gaali Lanti Plussu
+Chemadaalu Valichi Uthikarestayi Kottra Missu…
+
+Rayi Antu Ridingu Yelele
+Tuffanai Commingu Yelele
+Thutaala Strikingu Yelele
+Veedi Policingey Rules Breakingu..
+
+Jo Dhar Gaya Samjho MarGaya..
+
+GabbarSingh GabbarSingh
+He Z On The Way To Do Something..
+GabbarSingh GabbarSingh
+Its A Brand New Song To Sing
+
+GabbarSingh GabbarSingh
+He Z On The Way To Do Something..
+GabbarSingh GabbarSingh
+Its A Brand New Song To Sing
+Posted by sweety at 20:49 No comments: 
+Email This
+BlogThis!
+Share to Twitter
+Share to Facebook
+Share to Pinterest
+Labels: Gabbar Singh
+                    """)
+    elif choice == "8":
+        print("""\t\t\tEe reyi teeyanidi...Ee chirugaali manasainadi...
+Ee haayi maayanidi..intaku minchi emunnadi
+
+Evevo korikalu yedhalo jhummani antunnavi..
+aa konte mallikalu allana daagi vintunnavi...
+
+OO..varamula..dorikenee parichayam..
+Naa manasulo kurisene amrutham
+Naa .. niluvuna alalaye paravasam..
+Nee chelimi ke cheyani..ankitham..
+korukune teeramuga aagenu ee nimisham...
+
+Evevo korikalu yedhalo jhummani antunnavi..
+aa konte mallikalu allana daagi vintunnavi...
+
+Nee oopire vecchaga tagalani...
+Naa nuditipai tilakamai velagani..
+Naa choopule chalaga taakani..
+Nee pedavipai Navvuga nilavani..
+aashalake aayuvuga marenu nee sneham..
+
+Ee reyi tiyanidi....ee chirugali manasainadi...
+Ee hayi mayanidi....inthaku minchi emunnadi""")
+    elif choice == "9":
+        print("""\t\t\tNee chura chura chura chupule panja\n
+sala sala sala upire panja
+nara naramuna netture panja
+anuvanuvuna sattuve panja
+aduperagani vegame panja
+adarani penu dhairyame panja
+pedavanchuna mouname panja
+padunagu aalochane panja
+
+hey cheekatilo cheekatiga musina musuga nippulavante
+thappadane yuddamuga vekuva chudada repatikante
+aakasham nee panjaa
+adi gelavaali asalaina gunde dammuga
+aavesham nee panjaa
+adugeyyali chedunantham chese chaithanyangaa
+
+aatupotu leneleni sagarame untundaa
+yettupallam leneleni raadaarantu vundaa
+aakuraalani kommaremmalu chigurayye veelundaa
+yedemina thudivaraku edureeda saagali gaa..
+
+hey adugadugu alajadigaa nee jeevithame nee shathruvu kaaga
+bedirinche aapadane yedirinche gunamega panjaa
+
+aakasham nee panja
+adi gelavaali asalaina gunde dammuga
+aavesham nee panjaa
+adugeyyali chedunantham chese chaithanyangaa
+                    """)
+    elif choice == "10":
+        print("""\t\t\tye meraa jahaa ye mera ghar meraa aasiyaa
+ye meri duniyaa thera kaam kyaa hai yahaa
+ye meraa jahaa ye mera ghar meraa aasiyaa
+ye meri duniyaa thera kaam kyaa hai yahaa
+jeen ke andhere sooraj khole aa yahaan
+kaatonki raahe mile kaliyaa bichaathe wahaan
+chalthe masthi me chal jaayengi manjil kahaan
+kal aane waalonko dekh there kadam honge nishaa
+oh. pa pa pa ra pa pa pa pa pa
+oh. pa pa pa ra pa pa pa pa pa
+
+ithihaas ki shuruvaad aag se huyi
+aag shakthi hai aag jingdagi hai
+thum sabme yek jingaari hai jise koyi bhujaa nahi sakthaaa
+jo buraayi paas aayegi jal jaayegi jo paap kareeb aayegaa jal jaayegaa
+thum sab shole ho mashaale ho chalthe rahon jalthe rahon jalthe rahon
+oho oho meraa ghar mera aasiyaa
+oho oho theraa kaam kyaa hai yahaa
+ye meraa jahaa ye mera ghar meraa aasiyaa
+
+jeen ke andhere sooraj khole aa yahaan
+kaatonki raahe mile kaliyaa bichaathe wahaan
+chalthe masthi me chal jaayengi manjil kahaan
+kal aane waalonko dekh there kadam honge nishaa
+oh. pa pa pa ra pa pa pa pa pa
+oh. pa pa pa ra pa pa pa pa pa
+
+ithihaas ko badlaa pahiye nahi
+pahiyaa ghoomthaa hai duniyaa aage badhthi hai
+thumhe bhi ek din ithihaas ko badlnaa hai
+desh ko aage badhaanaa hai
+thum sab aanewaali kal ki saase ho
+thum sab aanewaali kal ki dhadkan ho
+thum ruk nahi sakthe dham nahi sakthe
+chalthe rahon chalthe rahon chalthe rahon
+oho oho meraa ghar mera aasiyaa
+oho oho theraa kaam kyaa hai yahaa
+
+ye meraa jahaa ye mera ghar meraa aasiyaa
+ye meri duniyaa thera kaam kyaa hai yahaa
+jeen ke andhere sooraj khole aa yahaan
+kaatonki raahe mile kaliyaa bichaathe wahaan
+chalthe masthi me chal jaayengi manjil kahaan
+aane waalonko dekh there kadam honge nishaa
+oho na na naa ra naa
+na na na ra na ra naa ra naa
+                    """)
+    
+
+    else:
+        print("This is Invalid song")
+        break
+    var = input("\t\t\tDo you want another song type y or n:")
+    if var == "y":
+        var =="y"
+        continue
+    elif var == "n":
+        break
+            
+
+
+
